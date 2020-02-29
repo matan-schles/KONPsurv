ae8b1939b2c1b192793e5571772baef5ad8447c4
matan-schles <matan.schles@gmail.com>
2020-02-29 20:30:26 +0200

    add carcinoma data to package

--- R/data.R
+++ R/data.R
@@ -18,3 +18,23 @@
 #' O., Mayer, R., et al. (1982). A comparison of combination chemotherapy and combined modality
 #' therapy for locally advanced gastricarcinoma. Cancer 49, 1771-1777.
 "gastric"
+
+
+
+#' Urothelial carcinoma.
+#'
+#' Survival data from a trial comparing chemotherapy versus atezolizumab in the treatment of Urothelial carcinoma.
+#'
+#' @usage data(carcinoma)
+#'
+#' @format A data frame with 625 observations (316 in the atezolizumab group and 309 chemotherapy group) with the following 3 columns:
+#' \describe{
+#'   \item{time}{the observed follow-up times in days.}
+#'   \item{status}{the event indicators, 0=right censored, 1= event.}
+#'   \item{group}{ the group labels, 1 = atezolizumab, 2 = chemotherapy.}
+#' }
+#' 
+#' @references Powles T, Dura?n I, van der Heijden MS, et al. Atezolizumab versus chemotherapy in patients with platinum-treated locally
+#' advanced or metastatic urothelial carcinoma (IMvigor211): a multicentre, open-label, phase 3 randomised controlled trial.
+#' Lancet 2018; 391: 748-757.
+"carcinoma"
